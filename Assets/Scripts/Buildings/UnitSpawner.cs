5a53931ea3ee5e94f4eb4590747c89812631d778
Jason Jaslow <jason@jaslow.com>
2020-11-09 18:09:53 -0500

    Unit Spawning

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,50 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+
+    [SerializeField] GameObject unitPrefab;
+    [SerializeField] Transform spawnPoint;
+
+
+
+    #region Server
+
+    [Command]
+    void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(unitPrefab, spawnPoint.position, spawnPoint.rotation);
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+
+    #endregion
+
+
+
+
+
+
+
+    #region Client
+
+    //detects clicks on items. Needs EventHandler game object
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
+
+        if (!hasAuthority)
+            return;
+
+        CmdSpawnUnit();
+    }
+
+
+    #endregion
+
+}
--- Assets/Scripts/Networking/MyNetworkManager.cs
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
+    [SerializeField] GameObject unitSpawnerPrefab;
 
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
 
+        GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 
 
 
